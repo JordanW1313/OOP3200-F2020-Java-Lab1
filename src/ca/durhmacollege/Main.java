ef64322230170bab01c64a9c20fcbfd0dbe264b7
JordanW1313 <jwriker@gmail.com>
2020-11-12 15:43:55 -0500

    initial commit

---
+++ src/ca/durhmacollege/Main.java
@@ -0,0 +1,19 @@
+/*
+* Group #5
+* Authors: Jordan Wriker Ahmed Rizvi
+* Assignment: Java Lab 1
+* Date: November 12, 2020
+* Purpose: Take three bowlers scores for two games and then display
+*          the information to the console.
+*
+*/
+
+
+package ca.durhmacollege;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
