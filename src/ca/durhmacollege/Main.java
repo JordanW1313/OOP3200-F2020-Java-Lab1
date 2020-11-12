75cdf6ebaa81d890a3a2621856ef1d29e4647982
JordanW1313 <jwriker@gmail.com>
2020-11-12 16:33:56 -0500

    defined scanner, arrays, and accepted user input

--- src/ca/durhmacollege/Main.java
+++ src/ca/durhmacollege/Main.java
@@ -11,9 +11,68 @@
 
 package ca.durhmacollege;
 
-public class Main {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        // Define arrays
+
+        String[] names = {"Jordan", "Ahmed", "Thom"};
+        int[][] scores = new int[3][2];
+
+        // Define our scanner
+
+        Scanner keyboard = new Scanner(System.in);
+
+        // Define score variable
+
+        int score;
+
+        // Input
+
+        for (int s = 0; s <= 1; s++)
+        {
+            for (int b = 0; b <= 2; b++)
+            {
+                System.out.print("Please enter " + names[b] + "'s score for GAME #" + (s + 1) + ": ");
+
+                boolean isValidInput = false;
+
+                while(!isValidInput)
+                {
+                    try
+                    {
+                        score = keyboard.nextInt();
+                        isValidInput = true;
+
+                        if((score < 0 ) || (score > 300))
+                        {
+                            System.out.println("Invalid input. Value between 0 and 300 needed. Please try again.");
+                            System.out.print("Please enter " + names[b] + "'s score for GAME #" + (s + 1) + ": ");
+                            keyboard.nextLine();
+                            isValidInput = false;
+                        }
+
+                        scores[b][s] = score;
+                    }
+                    catch(InputMismatchException e)
+                    {
+                        System.out.println("Invalid input. Whole number needed. Please try again.");
+                        System.out.print("Please enter " + names[b] + "'s score for GAME #" + (s + 1) + ": ");
+                        keyboard.nextLine();
+                        isValidInput = false;
+                    }
+                }
+            }
+            System.out.println("");
+        }
+
+        // Output
+
+
     }
 }
